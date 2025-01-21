503baef601ca1f427cd3afa7fac16ffc0c930607
diegoefraingonzal <13064@portalcedi.edu.mx>
2025-01-21 08:56:55 -0600

    Commit final

--- ea7/src/app/home/home.page.ts
+++ ea7/src/app/home/home.page.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  taskForm: FormGroup;
+  tasks: { name: string; date: string; completed: boolean }[] = [];
+
+  constructor(private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      name: ['', Validators.required],
+      date: ['', Validators.required],
+      completed: [false]
+    });
+  }
+
+  addTask() {
+    if (this.taskForm.valid) {
+      this.tasks.push({
+        name: this.taskForm.value.name,
+        date: this.taskForm.value.date,
+        completed: this.taskForm.value.completed
+      });
+      this.taskForm.reset();
+    }
+  }
+
+  toggleTaskCompletion(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
