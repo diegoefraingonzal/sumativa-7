bfea5eb4ebe3b69700dc15ed888cd3c58457789c
diegoefraingonzal <13064@portalcedi.edu.mx>
2025-01-21 08:17:25 -0600

    1er commit

---
+++ ea7/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ea7',
+  webDir: 'www'
+};
+
+export default config;
